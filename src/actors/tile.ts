620313837b6b2d0d35bc8468677a8fab1de12226
Adam Due Hansen <adamduehan@gmail.com>
2025-09-04 07:04:19 +0200

    Hero can only move to cell.

---
+++ src/actors/tile.ts
@@ -0,0 +1,100 @@
+import * as ex from "excalibur";
+import { Resources } from "../resources.ts";
+
+const spritesheet = ex.SpriteSheet.fromImageSource({
+  image: Resources.spritesheet,
+  grid: {
+    columns: 16,
+    rows: 10,
+    spriteHeight: 8,
+    spriteWidth: 8,
+  },
+  spacing: {
+    margin: {
+      x: 1,
+      y: 1,
+    },
+  },
+});
+
+export type TileData = [
+  ["", "", "", ""],
+  ["", "", "", ""],
+  ["", "", "", ""],
+  ["", "", "", ""],
+];
+
+interface CellArgs {
+  row: number;
+  column: number;
+}
+
+class Cell extends ex.Actor {
+  constructor(args: CellArgs) {
+    super({
+      name: "Cell",
+      height: 8,
+      width: 8,
+      color: ex.Color.Green,
+      pos: ex.vec(args.column * 8, args.row * 8),
+    });
+
+    this.graphics.use(spritesheet.getSprite(1, 1));
+  }
+}
+
+export class Tile extends ex.Actor {
+  #tileData: TileData;
+
+  constructor(tileData: TileData) {
+    super({
+      name: "Tile",
+    });
+    this.#tileData = tileData;
+  }
+
+  override onInitialize(_engine: ex.Engine): void {
+    for (let rowIndex = 0; rowIndex < this.#tileData.length; rowIndex++) {
+      const cellRow = this.#tileData[rowIndex];
+      for (let columnIndex = 0; columnIndex < cellRow.length; columnIndex++) {
+        const _cell = cellRow[columnIndex];
+        this.addChild(
+          new Cell({
+            row: rowIndex,
+            column: columnIndex,
+          }),
+        );
+      }
+    }
+  }
+
+  static #getAdjacentPos(
+    side: "top" | "right" | "bottom" | "left",
+    pos: ex.Vector,
+  ): ex.Vector | never {
+    switch (side) {
+      case "top": {
+        return ex.vec(pos.x, pos.y - 8 * 4);
+      }
+      case "right": {
+        return ex.vec(pos.x + 8 * 4, pos.y);
+      }
+      case "bottom": {
+        return ex.vec(pos.x, pos.y + 8 * 4);
+      }
+      case "left": {
+        return ex.vec(pos.x - 8 * 4, pos.y);
+      }
+    }
+  }
+
+  static adjacentTo(args: {
+    tile: Tile;
+    side: "top" | "right" | "bottom" | "left";
+    data: TileData;
+  }): Tile {
+    const newTile = new Tile(args.data);
+    newTile.pos = Tile.#getAdjacentPos(args.side, args.tile.pos);
+    return newTile;
+  }
+}
--- src/game-scene.ts
+++ src/game-scene.ts
@@ -1,5 +1,6 @@
 import * as ex from "excalibur";
 import { Resources } from "./resources.ts";
+import { Tile, TileData } from "./actors/tile.ts";
 // import { SocketManager } from "./socket-manager.ts";
 
 const spritesheet = ex.SpriteSheet.fromImageSource({
@@ -39,8 +40,17 @@ class Hero extends ex.Actor {
 
   private _makeDownHandler(): ex.Handler<ex.PointerEvent> {
     return (event) => {
-      this.pos = event.worldPos;
-      // TODO: Send new position to server
+      const clickedPosition = event.worldPos;
+      const selectedCell = this.scene?.actors.find((actor) =>
+        actor.name === "Cell" &&
+        actor.contains(clickedPosition.x, clickedPosition.y)
+      );
+
+      if (selectedCell === undefined) {
+        return;
+      }
+
+      this.pos = ex.vec(selectedCell.globalPos.x, selectedCell.globalPos.y);
     };
   }
 
@@ -68,94 +78,12 @@ class Hero extends ex.Actor {
 //   }
 // }
 
-type TileData = [
-  ["", "", "", ""],
-  ["", "", "", ""],
-  ["", "", "", ""],
-  ["", "", "", ""],
-];
-
 const tileData1: TileData = [
   ["", "", "", ""],
   ["", "", "", ""],
   ["", "", "", ""],
   ["", "", "", ""],
 ];
-
-interface CellArgs {
-  row: number;
-  column: number;
-}
-
-class Cell extends ex.Actor {
-  constructor(args: CellArgs) {
-    super({
-      name: "Cell",
-      height: 8,
-      width: 8,
-      color: ex.Color.Green,
-      pos: ex.vec(args.column * 8, args.row * 8),
-    });
-
-    this.graphics.use(spritesheet.getSprite(1, 1));
-  }
-}
-
-class Tile extends ex.Actor {
-  #tileData: TileData;
-
-  constructor(tileData: TileData) {
-    super({
-      name: "Tile",
-    });
-    this.#tileData = tileData;
-  }
-
-  override onInitialize(_engine: ex.Engine): void {
-    for (let rowIndex = 0; rowIndex < this.#tileData.length; rowIndex++) {
-      const cellRow = this.#tileData[rowIndex];
-      for (let columnIndex = 0; columnIndex < cellRow.length; columnIndex++) {
-        const _cell = cellRow[columnIndex];
-        this.addChild(
-          new Cell({
-            row: rowIndex,
-            column: columnIndex,
-          }),
-        );
-      }
-    }
-  }
-
-  static #getAdjacentPos(
-    side: "top" | "right" | "bottom" | "left",
-    pos: ex.Vector,
-  ): ex.Vector | never {
-    switch (side) {
-      case "top": {
-        return ex.vec(pos.x, pos.y - 8 * 4);
-      }
-      case "right": {
-        return ex.vec(pos.x + 8 * 4, pos.y);
-      }
-      case "bottom": {
-        return ex.vec(pos.x, pos.y + 8 * 4);
-      }
-      case "left": {
-        return ex.vec(pos.x - 8 * 4, pos.y);
-      }
-    }
-  }
-
-  static adjacentTo(args: {
-    tile: Tile;
-    side: "top" | "right" | "bottom" | "left";
-    data: TileData;
-  }): Tile {
-    const newTile = new Tile(args.data);
-    newTile.pos = Tile.#getAdjacentPos(args.side, args.tile.pos);
-    return newTile;
-  }
-}
 
 export class GameScene extends ex.Scene {
   #hero?: Hero;
